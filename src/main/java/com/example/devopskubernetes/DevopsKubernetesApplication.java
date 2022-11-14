9a31ace126eabcbd9623a0e145cc51be6941986b
PhucPham96 <phucpham0396@gmail.com>
2022-11-14 14:22:17 +0700

    first commit

---
+++ src/main/java/com/example/devopskubernetes/DevopsKubernetesApplication.java
@@ -0,0 +1,13 @@
+package com.example.devopskubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsKubernetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsKubernetesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devopskubernetes/DevopsKubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopskubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsKubernetesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
